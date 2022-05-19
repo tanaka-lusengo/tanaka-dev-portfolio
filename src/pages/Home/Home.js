7140f4432bf6a2bfef10a7d2bcb58f1563aaa46c
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-05-19 09:46:43 +0100

    add about and tech stack sections

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
+import About from "../../components/About/About";
 import Hero from "../../components/Hero/Hero";
-import Technologies from "../../components/Technologies/Technologies";
+import TechStack from "../../components/TechStack/TechStack";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <section>
       <Hero />
-      <Technologies />
+      <About />
+      <TechStack />
     </section>
   );
 };
