3dfcbdc6bdafc276beb4096c187afc85e4a75f2b
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-05-19 13:18:03 +0100

    add portfolio page, additional animation, add responsive design

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "../../components/About/About";
 import Hero from "../../components/Hero/Hero";
+import Portfolio from "../../components/Portfolio/Portfolio";
 import TechStack from "../../components/TechStack/TechStack";
 import "./Home.scss";
 
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <About />
       <TechStack />
+      <Portfolio />
     </section>
   );
 };
